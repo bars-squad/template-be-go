488d7641223eb681760c7409a0a4064710a972ac
Difa Al Fansha <difaal21@gmail.com>
2022-11-26 23:35:32 +0700

    feat(init): add dependency

--- main.go
+++ main.go
@@ -1,24 +1,32 @@
 package main
 
 import (
+	"context"
 	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
 
 	"github.com/difaal21/go-template/config"
+	"github.com/difaal21/go-template/databases/mongodb"
+	"github.com/difaal21/go-template/jwt"
+	"github.com/difaal21/go-template/middleware"
+	"github.com/difaal21/go-template/modules/admin"
 	"github.com/difaal21/go-template/responses"
 	"github.com/difaal21/go-template/server"
+	"github.com/go-playground/validator"
 	"github.com/gorilla/mux"
 	_ "github.com/joho/godotenv/autoload" //for development
 	"github.com/rs/cors"
 	"github.com/sirupsen/logrus"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 var (
-	cfg          *config.Config
-	httpResponse = responses.HttpResponse{}
-	messageIndex = "Application running properly"
+	cfg                 *config.Config
+	httpResponse        = responses.HttpResponseStatusCodesImpl{}
+	healthCheckMessage  = "Application running properly"
+	pageNotFoundMessage = "You're lost, double check the endpoint"
 )
 
 func init() {
@@ -30,13 +38,47 @@ func main() {
 	logger.SetFormatter(cfg.Logger.Formatter)
 	logger.SetReportCaller(true)
 
-	// basicAuth := middleware.NewBasicAuth(cfg.BasicAuth.Username, cfg.BasicAuth.Password)
+	validate := validator.New()
 
-	// validate := validator.New()
+	mongoClient, err := mongo.NewClient(cfg.Mongodb.ClientOptions)
+	if err != nil {
+		logger.Fatal(err)
+	}
+	mongoClientAdapter := mongodb.NewClientAdapter(mongoClient)
+	if err := mongoClientAdapter.Connect(context.Background()); err != nil {
+		logger.Fatal(err)
+	}
+
+	// set mongodb
+	mongodb := mongoClientAdapter.Database(cfg.Mongodb.Database)
+
+	// set jwt object
+	privateKey := jwt.GetRSAPrivateKey("./secret/private.pem")
+	publicKey := jwt.GetRSAPublicKey("./secret/public.pem")
+	jsonWebToken := jwt.NewJSONWebToken(privateKey, publicKey)
+
+	// set basic auth
+	basicAuth := middleware.NewBasicAuth(cfg.BasicAuth.Username, cfg.BasicAuth.Password)
+
+	sessionMiddleware := middleware.NewSessionMiddleware(jsonWebToken)
 
 	router := mux.NewRouter()
 	router.HandleFunc("/", index)
-	http.Handle("/", router)
+	// http.Handle("/", router)
+	router.NotFoundHandler = http.HandlerFunc(notFoundHandler)
+
+	adminRepository := admin.NewRepository(logger, mongodb)
+
+	adminUsecase := admin.NewUsecase(&admin.Property{
+		ServiceName:  cfg.Application.Name,
+		Logger:       logger,
+		Repository:   adminRepository,
+		JSONWebToken: jsonWebToken,
+		// Session:      sess,
+		// Publisher:                publisher,
+	})
+
+	admin.NewHTTPHandler(logger, validate, router, basicAuth, adminUsecase, sessionMiddleware)
 
 	handler := cors.New(cors.Options{
 		AllowedOrigins:   cfg.Application.AllowedOrigins,
@@ -45,8 +87,8 @@ func main() {
 		AllowCredentials: true,
 	}).Handler(router)
 
-	srv := server.NewServer(logger, handler, cfg.Application.Port)
-	srv.Start()
+	server := server.NewServer(logger, handler, cfg.Application.Port)
+	server.Start()
 
 	// When we run this program it will block waiting for a signal. By typing ctrl-C, we can send a SIGINT signal, causing the program to print interrupt and then exit.
 	sigterm := make(chan os.Signal, 1)
@@ -54,17 +96,16 @@ func main() {
 	<-sigterm
 
 	// closing service for a gracefull shutdown.
-	srv.Close()
-
+	server.Close()
+	// redis.Close()
+	// db.Close()
+	// publisher.Close()
 }
 
-// func index(w http.ResponseWriter, r *http.Request) {
-// 	response := responses.HttpResponseStatusCodesImpl{}
-// 	response.Ok("").NewResponses(nil, "Application running properly")
-// 	// resp := response.NewSuccessResponse(nil, response.StatOK, indexMessage)
-// 	// response.JSON(w, resp)
-// }
-
 func index(w http.ResponseWriter, r *http.Request) {
-	responses.JSON(w, httpResponse.Ok("").NewResponses(nil, messageIndex))
+	responses.REST(w, httpResponse.Ok("").NewResponses(nil, healthCheckMessage))
+}
+
+func notFoundHandler(w http.ResponseWriter, r *http.Request) {
+	responses.REST(w, httpResponse.NotFound("PAGES_NOT_FOUND").NewResponses(nil, pageNotFoundMessage))
 }
